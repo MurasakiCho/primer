c76c7e038c8bd347e7fcea95c696ff81720310a4
Sylrelessa <briana.oneal823@gmail.com>
2024-09-06 10:04:11 -0400

    calculateTotal function completed and all tests pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 export function calculateTotal (items, tax) {
   // TODO
+  let total = 0;
+
+  for(let i = 0; i < items.length; i++) {
+    if(items[i].taxable){
+      total += (items[i].price + (Math.abs(items[i].price * tax)))
+    } else{
+      total += items[i].price
+    }
+  }
+
+  return total;
 }
